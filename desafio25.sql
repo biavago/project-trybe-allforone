34e1a319dadea00ad68e87ba951ce6909386dbeb
beatriz <beatriz.evago@gmail.com>
2023-03-28 23:38:07 +0100

    desafios corrigidos

--- desafio25.sql
+++ desafio25.sql
@@ -1,2 +1,2 @@
 /* Delete todos os dados em que a unit_price da tabela order_details seja menor que 10.0000. */
-DELETE unit_price WHERE unit_price < 10.0000 FROM order_details;
+DELETE FROM order_details WHERE unit_price < 10.0000;
